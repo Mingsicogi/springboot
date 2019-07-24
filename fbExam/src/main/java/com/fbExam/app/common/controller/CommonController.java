d60e49f1ee6cbf71e37a48c53dc3f031064a9282
hyeongjunmin <alsgudwns007@naver.com>
2019-07-24 19:30:18 +0900

    basic project using thymeleaf, oracle db, hikari cp

--- fbExam/src/main/java/com/fbExam/app/common/controller/CommonController.java
+++ fbExam/src/main/java/com/fbExam/app/common/controller/CommonController.java
@@ -2,13 +2,17 @@ package com.fbExam.app.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 
 public class CommonController {
 	
-	@RequestMapping("/")
-	public String rootIndex() {
-		return "index";
+	@RequestMapping("/welcome")
+	public ModelAndView rootIndex(ModelAndView mav) {
+		
+		mav.setViewName("welcome");
+		
+		return mav;
 	}
 }
